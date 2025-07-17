7bd9ca3f9d685ecbba43e241316953d6ad25c1cc
dhruvraj-techsur <Dhruvraj.Rathore@techsur.solutions>
2025-07-16 22:21:52 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
+  server: {
+    port: 3000,
+    host: true
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
